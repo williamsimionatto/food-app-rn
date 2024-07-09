8202f1ac36ffb28d356a099841cc68ced0795a75
williamsimionatto <william.sn0226@gmail.com>
2024-07-09 11:21:50 -0300

    feat: create Banner Component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,17 @@ import { ScrollView, View } from "react-native";
 import Constants from "expo-constants";
 
 import { Header } from "../components/header";
+import { Banner } from "../components/banner";
 
 const statusBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
-    <ScrollView style={{ flex: 1 }} className="bg-slate-200" showsVerticalScrollIndicator={false}>
+    <ScrollView style={{ flex: 1 }} className="bg-slate-100" showsVerticalScrollIndicator={false}>
       <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
         <Header />
+
+        <Banner />
       </View>
     </ScrollView>
   );
