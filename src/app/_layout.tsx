5e7c415bcd50dce0cc7e9762f36898eb3ecfb2b3
williamsimionatto <william.sn0226@gmail.com>
2024-07-09 10:31:42 -0300

    chore: install and configure tailwind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,7 @@
-import { Stack } from "expo-router";
+import "../styles/global.css";
+
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+  return <Slot />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="w-full h-full flex justify-center items-center">
+      <Text className="text-xl text-emerald-700">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
